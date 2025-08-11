9bcf8c187b4021c9c7a754aff86cb1ccc9fbae4c
Name <you@example.com>
2025-08-11 23:02:37 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "4.39.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "rg" 
+    storage_account_name = "strgeg1qwefc"                                 
+    container_name       = "tfstate"                                  
+    key                  = "prod..tfstate"                
+  }
+}
+
+provider "azurerm" {
+  subscription_id = "98d972ed-e21e-446e-9886-a62d770b1fef"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "ado-rg"
+  location = "West Europe"
+}
